cbaa466811be97bae367da1cbf5d9b11428f40b4
Ivan Simonenko <simonenkoivansimon@gmail.com>
2025-11-17 14:40:34 +0300

    Получилось заменить на конус

--- lab3_new/main.py
+++ lab3_new/main.py
@@ -1,4 +1,3 @@
-# File: main.py
 import sys
 import numpy as np
 from pyglm import glm
@@ -7,58 +6,51 @@ from OpenGL.GLUT import *
 from shaders import DEPTH_VS, DEPTH_FS, SCENE_VS, SCENE_FS, create_program
 from utils import perspective, ortho, rotation_matrix
 from utils import set_mat4_uniform, draw_vao_elements, load_texture_file, print_controls
-from setup import generate_sphere_data, generate_cylinder_data, generate_floor_data, setup_object_vao_vbo, generate_torus_data  # <-- добавлен torus
+from setup import generate_cone_data, generate_cylinder_data, generate_floor_data, setup_object_vao_vbo, generate_torus_data
 
 class Scene:
     def __init__(self):
-        # Параметры окна и камеры
         self.window_width = 1200
         self.window_height = 800
         self.cam_rot_x = 30.0
         self.cam_rot_y = -30.0
         self.cam_distance = 1000.0
 
-        # Размеры и положение объектов
-        self.SPHERE_RADIUS = 120.0
+        self.cone_radius = 120.0
+        self.cone_height = 240.0
         self.cyl_height = 220.0
         self.cyl_radius = 70.0
-        self.torus_center = np.array([300.0, 125.0, 0.0], dtype=np.float32)  # место куба
-        self.sphere_center = np.array([-300.0, self.SPHERE_RADIUS, 0.0], dtype=np.float32)
-        self.cyl_center   = np.array([0.0, self.cyl_height/2.0, 0.0], dtype=np.float32)
+        self.torus_center = np.array([300.0, 125.0, 0.0], dtype=np.float32)
+        self.cone_center = np.array([-300.0, self.cone_height/2.0, 0.0], dtype=np.float32)
+        self.cyl_center = np.array([0.0, self.cyl_height/2.0, 0.0], dtype=np.float32)
 
-        # Параметры освещения
         self.light_enabled = True
         self.light_pos = [500.0, 500.0, 800.0, 1.0]
         self.light_intensity = 1.2
         self.light_diffuse = [1.0, 1.0, 1.0, 1.0]
         self.light_ambient = [0.08, 0.08, 0.08, 1.0]
 
-        # Цвета света (клавиши 1..5)
         self.LIGHT_COLOR_PRESETS = [
-            (1,0,0,1),    # красный
-            (0,0,1,1),    # синий
-            (0,1,0,1),    # зеленый
-            (1,1,0,1),    # желтый
-            (1,0,1,1),     # пурпурный
-            (1, 1, 1, 1)  # обычный белый свет
+            (1,0,0,1),
+            (0,0,1,1),
+            (0,1,0,1),
+            (1,1,0,1),
+            (1,0,1,1),
+            (1, 1, 1, 1)
         ]
 
-        # Текстура сферы
-        self.sphere_texture_enabled = True
-        self.sphere_texture_id = None
+        self.cone_texture_enabled = True
+        self.cone_texture_id = None
 
-        # Shadow map
         self.SHADOW_WIDTH, self.SHADOW_HEIGHT = 2048, 2048
         self.depthMapFBO = None
         self.depthMap = None
 
-        # Шейдерные программы
         self.shaderProgram = None
         self.depthShader = None
 
-        # VAO/VBO/EBO
-        self.sphere_VAO = self.sphere_VBO = self.sphere_EBO = None
-        self.sphere_num_indices = 0
+        self.cone_VAO = self.cone_VBO = self.cone_EBO = None
+        self.cone_num_indices = 0
         self.cyl_VAO = self.cyl_VBO = self.cyl_EBO = None
         self.cyl_num_indices = 0
         self.floor_VAO = self.floor_VBO = self.floor_EBO = None
@@ -71,12 +63,10 @@ class Scene:
         glEnable(GL_DEPTH_TEST)
         glDisable(GL_CULL_FACE)
 
-        # Шейдеры
         self.depthShader = create_program(DEPTH_VS, DEPTH_FS)
         self.shaderProgram = create_program(SCENE_VS, SCENE_FS)
         print("[INFO] Shaders compiled.")
 
-        # FBO и текстура для теней
         self.depthMapFBO = glGenFramebuffers(1)
         self.depthMap = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, self.depthMap)
@@ -99,30 +89,25 @@ class Scene:
             print("[INFO] Depth FBO OK")
         glBindFramebuffer(GL_FRAMEBUFFER, 0)
 
-        # Геометрия
-        # Сфера
-        sphere_verts, sphere_inds, sphere_count = generate_sphere_data(self.SPHERE_RADIUS, 64, 48)
-        self.sphere_VAO, self.sphere_VBO, self.sphere_EBO = setup_object_vao_vbo(sphere_verts, sphere_inds)
-        self.sphere_num_indices = sphere_count
-        # Цилиндр
+        cone_verts, cone_inds, cone_count = generate_cone_data(self.cone_radius, self.cone_height, 64)
+        self.cone_VAO, self.cone_VBO, self.cone_EBO = setup_object_vao_vbo(cone_verts, cone_inds)
+        self.cone_num_indices = cone_count
+
         cyl_verts, cyl_inds, cyl_count = generate_cylinder_data(self.cyl_radius, self.cyl_height, 64)
         self.cyl_VAO, self.cyl_VBO, self.cyl_EBO = setup_object_vao_vbo(cyl_verts, cyl_inds)
         self.cyl_num_indices = cyl_count
-        # Тор
+
         torus_verts, torus_inds, torus_count = generate_torus_data(120, 40, 48, 32)
         self.torus_VAO, self.torus_VBO, self.torus_EBO = setup_object_vao_vbo(torus_verts, torus_inds)
         self.torus_num_indices = torus_count
-        # Пол
+
         floor_verts, floor_inds, floor_count = generate_floor_data(2000, 10)
         self.floor_VAO, self.floor_VBO, self.floor_EBO = setup_object_vao_vbo(floor_verts, floor_inds)
         self.floor_num_indices = floor_count
 
-        # Текстура сферы
-        self.sphere_texture_id = load_texture_file("sphere_texture.jpg")
-
+        self.cone_texture_id = load_texture_file("sphere_texture.jpg")
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-
         print_controls()
 
     def compute_light_space_matrix(self):
@@ -136,16 +121,15 @@ class Scene:
         return lightProj * lightView
 
     def render_depth(self, prog):
-        # Пол
         set_mat4_uniform(prog, "model", glm.mat4(1.0))
         draw_vao_elements(self.floor_VAO, self.floor_EBO, self.floor_num_indices)
-        # Сфера
-        set_mat4_uniform(prog, "model", glm.translate(glm.mat4(1.0), glm.vec3(*self.sphere_center)))
-        draw_vao_elements(self.sphere_VAO, self.sphere_EBO, self.sphere_num_indices)
-        # Цилиндр
+
+        set_mat4_uniform(prog, "model", glm.translate(glm.mat4(1.0), glm.vec3(*self.cone_center)))
+        draw_vao_elements(self.cone_VAO, self.cone_EBO, self.cone_num_indices)
+
         set_mat4_uniform(prog, "model", glm.translate(glm.mat4(1.0), glm.vec3(*self.cyl_center)))
         draw_vao_elements(self.cyl_VAO, self.cyl_EBO, self.cyl_num_indices)
-        # Тор
+
         set_mat4_uniform(prog, "model", glm.translate(glm.mat4(1.0), glm.vec3(*self.torus_center)))
         draw_vao_elements(self.torus_VAO, self.torus_EBO, self.torus_num_indices)
 
@@ -160,6 +144,7 @@ class Scene:
 
         eff_intensity = self.light_intensity if self.light_enabled else 0.0
         eff_color = self.light_diffuse[:3] if self.light_enabled else [0.0, 0.0, 0.0]
+
         glUniform3fv(glGetUniformLocation(prog, "lightPos"), 1, self.light_pos[:3])
         glUniform3fv(glGetUniformLocation(prog, "lightColor"), 1, eff_color)
         glUniform1f(glGetUniformLocation(prog, "lightIntensity"), eff_intensity)
@@ -169,7 +154,6 @@ class Scene:
         glBindTexture(GL_TEXTURE_2D, self.depthMap)
         glUniform1i(glGetUniformLocation(prog, "shadowMap"), 1)
 
-        # --- Пол ---
         model = glm.mat4(1.0)
         set_mat4_uniform(prog, "model", model)
         glUniform3fv(glGetUniformLocation(prog, "materialDiffuse"), 1, [0.92, 0.92, 0.90])
@@ -179,20 +163,18 @@ class Scene:
         glUniform1i(glGetUniformLocation(prog, "isTransparent"), 0)
         draw_vao_elements(self.floor_VAO, self.floor_EBO, self.floor_num_indices)
 
-        # --- Сфера ---
-        set_mat4_uniform(prog, "model", glm.translate(glm.mat4(1.0), glm.vec3(*self.sphere_center)))
+        set_mat4_uniform(prog, "model", glm.translate(glm.mat4(1.0), glm.vec3(*self.cone_center)))
         glUniform3fv(glGetUniformLocation(prog, "materialSpecular"), 1, [0.05, 0.05, 0.05])
         glUniform1f(glGetUniformLocation(prog, "materialShininess"), 2.0)
-        if self.sphere_texture_enabled:
+        if self.cone_texture_enabled:
             glActiveTexture(GL_TEXTURE0)
-            glBindTexture(GL_TEXTURE_2D, self.sphere_texture_id)
+            glBindTexture(GL_TEXTURE_2D, self.cone_texture_id)
             glUniform1i(glGetUniformLocation(prog, "useTexture"), 1)
         else:
             glUniform1i(glGetUniformLocation(prog, "useTexture"), 0)
         glUniform1i(glGetUniformLocation(prog, "isTransparent"), 0)
-        draw_vao_elements(self.sphere_VAO, self.sphere_EBO, self.sphere_num_indices)
+        draw_vao_elements(self.cone_VAO, self.cone_EBO, self.cone_num_indices)
 
-        # --- Тор (красный, глянцевый) ---
         set_mat4_uniform(prog, "model", glm.translate(glm.mat4(1.0), glm.vec3(*self.torus_center)))
         glUniform3fv(glGetUniformLocation(prog, "materialDiffuse"), 1, [1.0, 0.0, 0.0])
         glUniform3fv(glGetUniformLocation(prog, "materialSpecular"), 1, [0.6, 0.6, 0.6])
@@ -201,7 +183,6 @@ class Scene:
         glUniform1i(glGetUniformLocation(prog, "isTransparent"), 0)
         draw_vao_elements(self.torus_VAO, self.torus_EBO, self.torus_num_indices)
 
-        # --- Цилиндр (синий, полупрозрачный) ---
         glDepthMask(GL_FALSE)
         glEnable(GL_BLEND)
         set_mat4_uniform(prog, "model", glm.translate(glm.mat4(1.0), glm.vec3(*self.cyl_center)))
@@ -213,27 +194,22 @@ class Scene:
         draw_vao_elements(self.cyl_VAO, self.cyl_EBO, self.cyl_num_indices)
         glDepthMask(GL_TRUE)
         glDisable(GL_BLEND)
-    # Остальные callback остаются прежними...
-
 
     def display(self):
         lightSpace = self.compute_light_space_matrix()
-        # --- Теневая карта ---
+
         glViewport(0, 0, self.SHADOW_WIDTH, self.SHADOW_HEIGHT)
         glBindFramebuffer(GL_FRAMEBUFFER, self.depthMapFBO)
         glClear(GL_DEPTH_BUFFER_BIT)
         glEnable(GL_POLYGON_OFFSET_FILL)
         glPolygonOffset(8.0, 32.0)
-
         glUseProgram(self.depthShader)
         set_mat4_uniform(self.depthShader, "lightSpaceMatrix", lightSpace)
         self.render_depth(self.depthShader)
         glUseProgram(0)
-
         glDisable(GL_POLYGON_OFFSET_FILL)
         glBindFramebuffer(GL_FRAMEBUFFER, 0)
 
-        # --- Основная сцена ---
         glViewport(0, 0, self.window_width, self.window_height)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glUseProgram(self.shaderProgram)
@@ -243,8 +219,8 @@ class Scene:
         up = glm.vec3(0.0, 1.0, 0.0)
         view = glm.lookAt(eye, center, up) * rotation_matrix(self.cam_rot_x, self.cam_rot_y)
         proj = perspective(50.0, self.window_width / float(self.window_height), 1.0, 5000.0)
-        self.render_scene(self.shaderProgram, view, proj, lightSpace)
 
+        self.render_scene(self.shaderProgram, view, proj, lightSpace)
         glUseProgram(0)
         glutSwapBuffers()
 
@@ -256,7 +232,7 @@ class Scene:
     def keyboard(self,key,x,y):
         k = key.decode() if isinstance(key, bytes) else key
         if k == '0':
-            self.sphere_texture_enabled = not self.sphere_texture_enabled
+            self.cone_texture_enabled = not self.cone_texture_enabled
         elif k in '123456':
             self.light_diffuse = self.LIGHT_COLOR_PRESETS[int(k)-1]
         elif k in ('=', '+'):
--- lab3_new/setup.py
+++ lab3_new/setup.py
@@ -1,33 +1,23 @@
-# File: setup.py
 import math
 import numpy as np
 import trimesh
 from OpenGL.GL import *
 
+def generate_cone_data(radius, height, slices):
+    cone = trimesh.creation.cone(radius=radius, height=height, sections=slices)
+    verts = cone.vertices
+    normals = cone.vertex_normals
+    inds = cone.faces.flatten()
 
-
-def generate_sphere_data(radius, slices, stacks):
-    # Генерация сферы через trimesh
-    sphere = trimesh.creation.uv_sphere(radius=radius, count=[stacks, slices])
-
-    verts = sphere.vertices
-    normals = sphere.vertex_normals
-    inds = sphere.faces.flatten()
-
-    # UV координаты через сферическую проекцию
     uvs = np.zeros((verts.shape[0], 2), dtype=np.float32)
     for i, (x, y, z) in enumerate(verts):
-        theta = math.atan2(z, x)  # угол вокруг Y
-        phi = math.acos(y / radius)  # угол от верхнего полюса
-        u = (theta + math.pi) / (2 * math.pi)  # 0..1
-        v = phi / math.pi  # 0..1
+        theta = math.atan2(z, x)
+        u = (theta + math.pi) / (2 * math.pi)
+        v = (y + height / 2) / height
         uvs[i] = [u, v]
 
-    # Объединяем в формат [x, y, z, nx, ny, nz, u, v]
     verts_with_data = np.hstack([verts, normals, uvs]).astype(np.float32)
-
     return verts_with_data, np.array(inds, dtype=np.uint32), len(inds)
-
 
 def generate_cylinder_data(radius, height, slices):
     cyl = trimesh.creation.cylinder(radius=radius, height=height, sections=slices)
@@ -35,41 +25,28 @@ def generate_cylinder_data(radius, height, slices):
     normals = cyl.vertex_normals
     inds = cyl.faces.flatten()
 
-    # UV координаты
     uvs = np.zeros((verts.shape[0], 2), dtype=np.float32)
-    # Для боковой поверхности: u по углу, v по высоте
     for i, (x, y, z) in enumerate(verts):
-        # Определяем угол по XZ
         theta = math.atan2(z, x)
-        u = (theta + math.pi) / (2 * math.pi)  # 0..1
-        v = (y + height / 2) / height  # 0..1
+        u = (theta + math.pi) / (2 * math.pi)
+        v = (y + height / 2) / height
         uvs[i] = [u, v]
-    # Объединяем в формат [x, y, z, nx, ny, nz, u, v]
+
     verts_with_data = np.hstack([verts, normals, uvs]).astype(np.float32)
     return verts_with_data, np.array(inds, dtype=np.uint32), len(inds)
 
-
-# Генерация плоскости (пол)
 def generate_floor_data(size, repeat_tex=10):
-    half = size / 2.0  # половина размера
-    # вершины плоскости (позиция, нормаль, UV)
+    half = size / 2.0
     verts = np.array([
         -half, 0.0, -half, 0.0, 1.0, 0.0, 0.0, repeat_tex,
         half, 0.0, -half, 0.0, 1.0, 0.0, repeat_tex, repeat_tex,
         half, 0.0, half, 0.0, 1.0, 0.0, repeat_tex, 0.0,
         -half, 0.0, half, 0.0, 1.0, 0.0, 0.0, 0.0
     ], dtype=np.float32)
-    inds = np.array([0, 1, 2, 2, 3, 0], dtype=np.uint32)  # два треугольника
+    inds = np.array([0, 1, 2, 2, 3, 0], dtype=np.uint32)
     return verts, inds, len(inds)
 
 def generate_torus_data(radius_major, radius_minor, radial_segments, tubular_segments):
-    """
-    radius_major  – большой радиус тора (от центра до середины трубы)
-    radius_minor  – малый радиус (толщина трубы)
-    radial_segments – сегменты вокруг центра
-    tubular_segments – сегменты трубы
-    """
-
     vertices = []
     normals = []
     uvs = []
@@ -83,7 +60,6 @@ def generate_torus_data(radius_major, radius_minor, radial_segments, tubular_seg
             phi = (j / tubular_segments) * 2 * math.pi
             next_phi = ((j + 1) % tubular_segments) / tubular_segments * 2 * math.pi
 
-            # Создаем 4 точки квадрата (i,j) - (i,j+1)
             def point(a, b):
                 x = (radius_major + radius_minor * math.cos(b)) * math.cos(a)
                 y = radius_minor * math.sin(b)
@@ -95,7 +71,6 @@ def generate_torus_data(radius_major, radius_minor, radial_segments, tubular_seg
             p3 = point(next_theta, phi)
             p4 = point(next_theta, next_phi)
 
-            # нормали — от центра трубы
             def normal(a, b):
                 nx = math.cos(a) * math.cos(b)
                 ny = math.sin(b)
@@ -107,20 +82,17 @@ def generate_torus_data(radius_major, radius_minor, radial_segments, tubular_seg
             n3 = normal(next_theta, phi)
             n4 = normal(next_theta, next_phi)
 
-            # UV
             u1, v1 = i / radial_segments, j / tubular_segments
             u2, v2 = i / radial_segments, (j + 1) / tubular_segments
             u3, v3 = (i + 1) / radial_segments, j / tubular_segments
             u4, v4 = (i + 1) / radial_segments, (j + 1) / tubular_segments
 
             idx = len(vertices) // 3
-
             vertices.extend(p1); normals.extend(n1); uvs.extend([u1, v1])
             vertices.extend(p2); normals.extend(n2); uvs.extend([u2, v2])
             vertices.extend(p3); normals.extend(n3); uvs.extend([u3, v3])
             vertices.extend(p4); normals.extend(n4); uvs.extend([u4, v4])
 
-            # два треугольника
             indices += [
                 idx, idx + 1, idx + 2,
                 idx + 2, idx + 1, idx + 3
@@ -135,29 +107,29 @@ def generate_torus_data(radius_major, radius_minor, radial_segments, tubular_seg
 
     return verts_with_data, indices, len(indices)
 
-
-# Настройка VAO/VBO/EBO для объекта
 def setup_object_vao_vbo(vertices_data, indices_data=None):
-    vao = glGenVertexArrays(1)  # генерируем VAO
-    vbo = glGenBuffers(1)  # генерируем VBO
-    glBindVertexArray(vao)  # активируем VAO
+    vao = glGenVertexArrays(1)
+    vbo = glGenBuffers(1)
+    glBindVertexArray(vao)
     glBindBuffer(GL_ARRAY_BUFFER, vbo)
     glBufferData(GL_ARRAY_BUFFER, vertices_data.nbytes, vertices_data, GL_STATIC_DRAW)
+
     ebo = None
     if indices_data is not None:
-        ebo = glGenBuffers(1)  # генерируем EBO
+        ebo = glGenBuffers(1)
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo)
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices_data.nbytes, indices_data, GL_STATIC_DRAW)
-    # Позиция (layout = 0)
+
     glEnableVertexAttribArray(0)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * 4,
-                          ctypes.c_void_p(0))  # 8(pos(3) + normal(3) + uv(2)) * sizeof(float)
-    # Нормаль (layout = 1)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * 4, ctypes.c_void_p(0))
+
     glEnableVertexAttribArray(1)
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * 4, ctypes.c_void_p(3 * 4))
-    # Текстурные координаты (layout = 2)
+
     glEnableVertexAttribArray(2)
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * 4, ctypes.c_void_p(6 * 4))
-    glBindVertexArray(0)  # отвязываем VAO
-    glBindBuffer(GL_ARRAY_BUFFER, 0)  # отвязываем VBO
+
+    glBindVertexArray(0)
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+
     return vao, vbo, ebo
